f1db288b41ebfe239f3e5f894b4595545f6055f7
David White <dwhite0627@gmail.com>
2020-01-10 21:36:06 -0500

    Create Characters component

---
+++ starwars/src/components/CharacterComponents/Characters.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {CardDeck, Row, CardGroup, CardColumns} from "reactstrap";
+import Character from './Character';
+
+const Characters = (props) => {
+  const [charState, setCharState] = useState([]);
+
+  useEffect(() => {
+    // api information
+    const apiURL = 'https://swapi.co/api/people/';  
+    axios
+      .get(apiURL)
+      .then(res => {
+        setCharState(res.data.results);
+      })
+      .catch(err => console.log(err));
+  }, []);
+  console.log(charState);
+
+  if (!charState) return <h3>Loading...</h3>;
+
+  return (
+      <CardDeck>
+        {charState.map((char, index) => <Character key={index} data={char} />)}
+      </CardDeck>
+  );
+};
+
+export default Characters;
