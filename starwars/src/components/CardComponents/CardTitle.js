9da65cbb79cbd52ad9c899c2e394438d51930083
David White <dwhite0627@gmail.com>
2020-01-10 20:03:55 -0500

    Create styled components for card text, subtitle and title

---
+++ starwars/src/components/CardComponents/CardTitle.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const CardTitle = styled.h2`
+  font-size: 2rem;
+  padding: 0;
+  margin: 2%;
+
+  @media (max-width: 950px) {
+    font-size: 1.6rem;
+  }
+
+  @media (max-width: 750px) {
+    font-size: 1rem;
+  }
+
+  @media (max-width: 550px) {
+    font-size: 0.4rem;
+  }
+`;
+
+export default CardTitle;
