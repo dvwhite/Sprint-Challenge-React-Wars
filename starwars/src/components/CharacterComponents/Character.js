bddeac78f626ba5316f2eb513768272da43dc0e5
David White <dwhite0627@gmail.com>
2020-01-10 20:35:57 -0500

    Add reactstrap Card styles

--- starwars/src/components/CharacterComponents/Character.js
+++ starwars/src/components/CharacterComponents/Character.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Card, CardBody, CardImg} from "reactstrap";
-import CardTitle from './../CardComponents/CardTitle';
-import CardSubtitle from './../CardComponents/CardSubtitle';
-import CardTxt from './../CardComponents/CardTxt';
+import {Card, CardBody, CardTitle, CardSubtitle, CardText} from "reactstrap";
+// import CardTitle from './../CardComponents/CardTitle';
+// import CardSubtitle from './../CardComponents/CardSubtitle';
+// import CardTxt from './../CardComponents/CardTxt';
 
 const Character = ({data}) => {
   return (
@@ -10,11 +10,11 @@ const Character = ({data}) => {
       <CardBody>
         <CardTitle>{data.name}</CardTitle>
         <CardSubtitle>{`Height: ${data.height}`}</CardSubtitle>
-        <CardTxt>{`Mass: ${data.mass}`}</CardTxt>
-        <CardTxt>{`Hair color: ${data.hair_color}`}</CardTxt>
-        <CardTxt>{`Eye color: ${data.eye_color}`}</CardTxt>
-        <CardTxt>{`Birth year: ${data.birth_year}`}</CardTxt>
-        <CardTxt>{`Gender: ${data.gender}`}</CardTxt>
+        <CardText>{`Mass: ${data.mass}`}</CardText>
+        <CardText>{`Hair color: ${data.hair_color}`}</CardText>
+        <CardText>{`Eye color: ${data.eye_color}`}</CardText>
+        <CardText>{`Birth year: ${data.birth_year}`}</CardText>
+        <CardText>{`Gender: ${data.gender}`}</CardText>
       </CardBody>
     </Card>
   );
